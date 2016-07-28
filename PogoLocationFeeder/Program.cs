44431cebda5f387b3fa5d7f645429daf1f278a34
5andr0 <sandro.trianni@gmail.com>
2016-07-29 00:37:27 +0200

    Project template

---
+++ PogoLocationFeeder/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+using Discord;
+
+namespace PogoLocationFeeder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
