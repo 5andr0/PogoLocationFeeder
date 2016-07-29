078ff76fcd7ee6dd79a67c6feb2655a09a8b2534
5andr0 <sandro.trianni@gmail.com>
2016-07-29 03:39:37 +0200

    Sending Json Formatted PokeInfo
    This is super ugly right now and all in one for testing. Refactoring
    later

--- PogoLocationFeeder/Program.cs
+++ PogoLocationFeeder/Program.cs
@@ -6,15 +6,17 @@ using System.Threading.Tasks;
 using System.Text.RegularExpressions;
 using Discord;
 using POGOProtos.Enums;
+using System.Net.Sockets;
+using System.Net;
+using System.Threading;
+using System.Collections;
+using System.IO;
+using Newtonsoft.Json;
 
 namespace PogoLoco
 {
     public class PokemonInfo
     {
-        public PokemonInfo()
-        {
-        }
-        
         public double latitude { get; set; }
         public double longitude { get; set; }
         public double iv { get; set; }
@@ -26,20 +28,82 @@ namespace PogoLoco
     {
         static void Main(string[] args) => new Program().Start();
 
+        private TcpListener listener;
+        ArrayList arrSocket = new ArrayList();
+
+        bool SocketConnected(Socket s)
+        {
+            bool part1 = s.Poll(1000, SelectMode.SelectRead);
+            bool part2 = (s.Available == 0);
+            if (part1 && part2)
+                return false;
+            else
+                return true;
+        }
+        public void StartNet()
+        {
+            listener = new TcpListener(IPAddress.Any, 16969);
+            listener.Start();
+            Console.WriteLine("Listening...");
+            StartAccept();
+        }
+        private void StartAccept()
+        {
+            listener.BeginAcceptTcpClient(HandleAsyncConnection, listener);
+        }
+        private void HandleAsyncConnection(IAsyncResult res)
+        {
+            StartAccept();
+            TcpClient client = listener.EndAcceptTcpClient(res);
+            if (client != null && SocketConnected(client.Client))
+            {
+                arrSocket.Add(client);
+                Console.WriteLine("new connection");
+            }
+        }
+
         private DiscordClient _client;
 
         private async Task feedToClients(PokemonInfo info)
         {
+            foreach (TcpClient obj in arrSocket) // Repeat for each connected client (socket held in a dynamic array)
+            {
+                TcpClient socket = obj;
+
+                if (!SocketConnected(socket.Client))
+                {
+                    //arrSocket.Remove(obj); // this is crashing, needs a fix
+                    continue;
+                }
+
+                NetworkStream networkStream = socket.GetStream();
+
+                JsonTextWriter jsonWriter = new JsonTextWriter(new StreamWriter(networkStream));
+
+                JsonSerializer ser = new JsonSerializer();
+                ser.Serialize(jsonWriter, info);
+                jsonWriter.Flush();
+            }
+
+            // debug output
             Console.WriteLine(info.latitude);
             Console.WriteLine(info.longitude);
             Console.WriteLine(info.iv);
             Console.WriteLine(info.timeStamp);
             Console.WriteLine(info.id);
+
+            /*
+            var obj2 = _client.Servers;
+            foreach (Server s in obj2)
+            {
+                Console.WriteLine($"{s.ChannelCount} {s.DefaultChannel} {s.Id} {s.Name}");
+            }*/
+
         }
         private async Task parseMessage(string message)
         {
             PokemonInfo info = new PokemonInfo();
-            //var test = "[378 seconds remaining] 100.8% IV - snorlax at 51.537633610483,-0.035369255277754 [ Moveset: LickFast/Earthquake ]\nJynx 90IV 40.677105409698, -73.446518725481";
+            var a = "[378 seconds remaining] 100.8% IV - snorlax at 51.537633610483,-0.035369255277754 [ Moveset: LickFast/Earthquake ]\nJynx 90IV 40.677105409698, -73.446518725481";
 
             var lines = message.Split(new[] { '\r', '\n' });
             foreach (var input in lines)
@@ -50,7 +114,8 @@ namespace PogoLoco
                     {
                         info.latitude = Convert.ToDouble(match.Groups["lat"].Value);
                         info.longitude = Convert.ToDouble(match.Groups["long"].Value);
-                    } else
+                    }
+                    else
                     {
                         Console.WriteLine($"Can't get coords from line: {lines}");
                         continue;
@@ -75,7 +140,8 @@ namespace PogoLoco
 
                 foreach (string name in Enum.GetNames(typeof(PokemonId)))
                 {
-                    if (message.IndexOf(name, StringComparison.OrdinalIgnoreCase) >= 0) { 
+                    if (input.IndexOf(name, StringComparison.OrdinalIgnoreCase) >= 0)
+                    {
                         info.id = (PokemonId)Enum.Parse(typeof(PokemonId), name);
                     }
                 }
@@ -89,12 +155,15 @@ namespace PogoLoco
             var server = "PoGo Sniping";
             var channel = "snipers";
             var discordToken = "MjA4MzE2MjQxOTQzNzI0MDMz.Cnv5Hg.JpdTfgfIdw5445Cnnu6QbLHnZDc";
-            
+
             _client = new DiscordClient();
+
+            StartNet();
 
             _client.MessageReceived += async (s, e) =>
             {
-                if (!e.Message.IsAuthor && e.Server.ToString() == server && e.Channel.ToString() == channel) {
+                if (!e.Message.IsAuthor && e.Server.ToString() == server && e.Channel.ToString() == channel)
+                {
                     await parseMessage(e.Message.Text);
                 }
             };
