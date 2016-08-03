20323fe476aca2cb56f9b5b233e0a5f9c7239c0a
5andr0 <sandro.trianni@gmail.com>
2016-08-03 14:24:40 +0200

    New release version

--- PogoLocationFeeder.GUI/Properties/AssemblyInfo.cs
+++ PogoLocationFeeder.GUI/Properties/AssemblyInfo.cs
@@ -52,5 +52,5 @@ using System.Windows;
 // übernehmen, indem Sie "*" eingeben:
 // [assembly: AssemblyVersion("1.0.*")]
 
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.1.3.0")]
+[assembly: AssemblyFileVersion("0.1.3.0")]
