6ebb9ca491de385423e7c1d36cfa66482fda484a
ExcuseMi <wardje@gmail.com>
2016-08-08 14:35:31 +0200

    Fix error when unserializing json object

--- PogoLocationFeeder/Helper/JsonSerializerSettingsFactory.cs
+++ PogoLocationFeeder/Helper/JsonSerializerSettingsFactory.cs
@@ -3,8 +3,10 @@ using System.Collections.Generic;
 using System.Globalization;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 
 namespace PogoLocationFeeder.Helper
 {
@@ -13,6 +15,46 @@ namespace PogoLocationFeeder.Helper
         public JsonSerializerSettingsCultureInvariant()
         {
             Culture = CultureInfo.InvariantCulture;
+            Converters = new List<JsonConverter> { new DoubleConverter()};
         }
     }
+
+    public class DoubleConverter : JsonConverter
+    {
+        public override bool CanConvert(Type objectType)
+        {
+            return (objectType == typeof(double) || objectType == typeof(double?));
+        }
+
+        public override object ReadJson(JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer)
+        {
+            JToken token = JToken.Load(reader);
+            if (token.Type == JTokenType.Float || token.Type == JTokenType.Integer)
+            {
+                return token.ToObject<double>();
+            }
+            if (token.Type == JTokenType.String)
+            {
+                var match = Regex.Match(token.ToString(), @"(1?\-?\d+\.?\d*)");
+                if (match.Success)
+                {
+                    return Double.Parse(match.Groups[1].Value, System.Globalization.CultureInfo.InvariantCulture);
+                }
+                return Double.Parse(token.ToString(),
+                       System.Globalization.CultureInfo.InvariantCulture);
+            }
+            if (token.Type == JTokenType.Null && objectType == typeof(double?))
+            {
+                return null;
+            }
+            throw new JsonSerializationException("Unexpected token type: " +
+                                                  token.Type.ToString());
+        }
+
+        public override void WriteJson(JsonWriter writer, object value, JsonSerializer serializer)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
 }
