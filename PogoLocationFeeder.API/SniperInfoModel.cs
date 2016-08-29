9830ed86e76a8be7416b7b7aacd5f5d5656d25de
ExcuseMi <wardje@gmail.com>
2016-08-30 00:53:24 +0200

    Update API model

--- PogoLocationFeeder.API/SniperInfoModel.cs
+++ PogoLocationFeeder.API/SniperInfoModel.cs
@@ -26,17 +26,18 @@ namespace PogoLocationFeeder.API
     {
         [JsonProperty("ExpirationTimestamp")]
         public DateTime ExpirationTimestamp { get; set; }
-
         [JsonProperty("Latitude")]
         public double Latitude { get; set; }
-
         [JsonProperty("Longitude")]
         public double Longitude { get; set; }
-
         [JsonProperty("Id")]
         public PokemonId Id { get; set; } = PokemonId.Missingno;
-
         [JsonProperty("IV")]
         public double IV { get; set; }
+        [JsonProperty("Move1")]
+        public PokemonMove Move1 { get; set; }
+        [JsonProperty("Move2")]
+        public PokemonMove Move2 { get; set; }
+
     }
 }
